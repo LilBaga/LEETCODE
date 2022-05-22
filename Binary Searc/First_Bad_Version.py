681c52bc9e3186970989d2d743c4e46d5e0e3a02
Aidana Baglanova <89396938+LilBaga@users.noreply.github.com>
2022-05-22 21:33:02 +0600

    Create First_Bad_Version.py

---
+++ Binary Searc/First_Bad_Version.py
@@ -0,0 +1,16 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        left = 1
+        right = n
+        if n == 1:
+            return 1
+        while (left<right):
+            mid = left+(right-left)//2
+            if (isBadVersion(mid)==True):
+                right = mid
+            else:
+                left = mid+1
+        return left
