30aa7b669ec516a93dd19b7865404fd46aee5315
Aidana Baglanova <89396938+LilBaga@users.noreply.github.com>
2022-10-03 01:39:19 +0600

    Update and rename number-of-dice-rolls-with-target-sum to number-of-dice-rolls-with-target-sum.py

--- octdaily2022/number-of-dice-rolls-with-target-sum.py
+++ octdaily2022/number-of-dice-rolls-with-target-sum.py
@@ -1,4 +1,4 @@
-number-of-dice-rolls-with-target-sum link:https://leetcode.com/problems/number-of-dice-rolls-with-target-sum/description/
+#number-of-dice-rolls-with-target-sum link:https://leetcode.com/problems/number-of-dice-rolls-with-target-sum/description/
 class Solution:
     def numRollsToTarget(self, d: int, f: int, target: int) -> int:
          @lru_cache(maxsize=None)
